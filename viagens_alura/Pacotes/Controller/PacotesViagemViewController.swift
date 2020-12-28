e081b6d65938c2e8b6c238458db0ef9230360d59
Breno <cintrabreno22@gmail.com>
2020-12-28 10:33:46 -0300

    adicionando collection view

--- viagens_alura/Pacotes/Controller/PacotesViagemViewController.swift
+++ viagens_alura/Pacotes/Controller/PacotesViagemViewController.swift
@@ -8,29 +8,53 @@
 
 import UIKit
 
-class PacotesViagemViewController: UIViewController, UICollectionViewDataSource {
+class PacotesViagemViewController: UIViewController, UICollectionViewDataSource,  UICollectionViewDelegateFlowLayout {
  
     
     @IBOutlet weak var colecaoPacotesViagem: UICollectionView!
     
+    let listaViagens: Array<Viagem> = ViagemDAO().retornaTodasAsViagens()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        colecaoPacotesViagem.dataSource = self
+        colecaoPacotesViagem.delegate = self
 
     }
     
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        return self.listaViagens.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         let celulaPacote = collectionView.dequeueReusableCell(withReuseIdentifier: "celulaPacote", for: indexPath)as! PacoteViagemCollectionViewCell
         
-        celulaPacote.backgroundColor = UIColor.red
+        let viagemAtual = listaViagens[indexPath.item]
+        
+        celulaPacote.labelTitulo?.text = viagemAtual.titulo
+        celulaPacote.labelQuantidadeDias?.text = "\(viagemAtual.quantidadeDeDias) dias"
+        celulaPacote.labelPreco?.text = "R$\(viagemAtual.preco)"
+        celulaPacote.imagemViagem.image = UIImage(named: viagemAtual.caminhoDaImagem)
+        
+        celulaPacote.layer.borderWidth = 0.5
+        celulaPacote.layer.borderColor = UIColor(displayP3Red: 85.0/255.0, green: 85.0/255.0, blue: 85.0/255.0, alpha: 1).cgColor
+        celulaPacote.layer.cornerRadius = 8
+        
         
         return celulaPacote
 
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        if let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            layout.estimatedItemSize = .zero
+        }
+        
+        let larguraCelula = collectionView.bounds.width / 2
+        return CGSize(width: larguraCelula - 15, height: 260)
     }
 
 }
